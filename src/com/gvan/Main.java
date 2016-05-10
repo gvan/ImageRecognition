8ece24e8d51e184b460be7db4f55cb6c7c9942c1
gvan <gvanjustfun@gmail.com>
2016-05-10 18:41:12 +0300

    morphologic

--- src/com/gvan/Main.java
+++ src/com/gvan/Main.java
@@ -4,15 +4,16 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String fileName = "/home/ivan/Study/diplom/images/binary.pgm";
-//        String fileName = "/home/betinvest/Study/diplom/images/test.pgm";
+//        String fileName = "/home/ivan/Study/diplom/images/binary.pgm";
+        String fileName = "/home/betinvest/Study/diplom/images/test.pgm";
 
         Image image = new Image(fileName);
         image.toBinary(image.intensity/2);
-//        ObjectCounter objectCounter = new ObjectCounter();
-//        Utils.log("object count %s", objectCounter.getObjectCount(image));
-        ConnectedComponent connectedComponent = new ConnectedComponent(image);
-        connectedComponent.classicalConnect().printImage();
+        image.printBinaryImage();
+
+        Morphologic morphologic = new Morphologic();
+        morphologic.initStruct(5, 5);
+        morphologic.breaking(image).printBinaryImage();
 
     }
 
