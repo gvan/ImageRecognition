f9f17985048b32e7c86b693d2273f1d7a3345e85
Ivan Ganzha <gvanjustfun@gmail.com>
2016-05-30 01:14:33 +0300

    find patterns

--- src/com/gvan/Main.java
+++ src/com/gvan/Main.java
@@ -4,14 +4,16 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String fileName = "/home/ivan/Study/diplom/images/baboon.pgm";
+        String fileName = "/home/ivan/Study/diplom/images/qr.pgm";
         String outFileName = "/home/ivan/Study/diplom/images/binarization.pgm";
 //        String fileName = "/home/betinvest/Study/diplom/images/test.pgm";
 
         Image image = new Image(fileName);
         Binarization binarization = new Binarization();
-        binarization.binarize(image).saveFile(outFileName);
-
+        image = binarization.binarize(image);
+//        image.saveFile(outFileName);
+        QrReader qrReader = new QrReader();
+        qrReader.recognizeFindPatterns(image);
     }
 
 }
